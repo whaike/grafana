a6d002b97d1b33b6dcbb97c6bca75ecb5505d4bb
Ivana <ivana.huckova@gmail.com>
2021-07-28 10:43:27 +0200

    Make Show context visible when no wrapping line

--- packages/grafana-ui/src/components/Logs/LogRowMessage.tsx
+++ packages/grafana-ui/src/components/Logs/LogRowMessage.tsx
@@ -52,7 +52,7 @@ const getStyles = stylesFactory((theme: GrafanaTheme) => {
       white-space: pre;
     `,
     contextNewline: css`
-      display: inline-block;
+      display: block;
     `,
   };
 });
